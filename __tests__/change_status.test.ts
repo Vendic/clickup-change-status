9611e04382e978e5730fcd436e919dffb10d8253
Tjitse Efdé <tjitse@vendic.nl>
2021-08-11 17:12:58 +0200

    Create change_status.ts

---
+++ __tests__/change_status.test.ts
@@ -0,0 +1,48 @@
+// Mocking the github context https://github.com/actions/toolkit/blob/master/docs/github-package.md#mocking-the-github-context
+
+import * as core from '@actions/core'
+import run from '../change_status'
+import nock from "nock";
+
+describe('Test happy path', () => {
+    it('does a call to the Clickup API', async () => {
+        // Mocks
+        const infoMock = jest.spyOn(core, 'info')
+        const apiReply = {
+            "id": "9hx",
+            "custom_id": null,
+            "name": "Updated Task Name",
+            "text_content": "Updated Task Content",
+            "description": "Updated Task Content",
+            "status": {
+                "status": "in review",
+                "color": "#d3d3d3",
+                "orderindex": 1,
+                "type": "custom"
+            }
+        }
+        nock('https://api.clickup.com')
+            .persist()
+            .put('/api/v2/task/ABC-123/?custom_task_ids=true&team_id=123')
+            .reply(200, apiReply)
+
+
+        await run()
+
+        // Assertions
+        expect(infoMock).toHaveBeenCalledWith('Changed the status of ABC-123 to in review successfully.')
+    })
+})
+
+beforeEach(() => {
+    jest.resetModules()
+    process.env['INPUT_CLICKUP_TOKEN'] = 'xyz'
+    process.env['INPUT_CLICKUP_CUSTOM_TASK_ID'] = 'ABC-123'
+    process.env['INPUT_CLICKUP_TEAM_ID'] = '123'
+    process.env['INPUT_CLICKUP_STATUS'] = 'in review'
+})
+
+afterEach(() => {
+    delete process.env['GITHUB_REPOSITORY']
+    delete process.env['INPUT_TOKEN']
+})
--- change_status.ts
+++ change_status.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+// import * as github from '@actions/github'
+import axios, { AxiosResponse } from 'axios'
 
 const run = async (): Promise<void> => {
     try {
@@ -7,12 +8,20 @@ const run = async (): Promise<void> => {
         const task_id = core.getInput('clickup_custom_task_id')
         const team_id = core.getInput('clickup_team_id')
         const target_status = core.getInput('clickup_status')
-        const put_body:JSON = <JSON><unknown>{
+        const body = {
             "status": target_status
         }
+        const endpoint = `https://api.clickup.com/api/v2/task/${task_id}/?custom_task_ids=true&team_id=${team_id}`
 
+        let result: AxiosResponse = await axios.put(endpoint, body, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+        let new_status = result.data.status.status
 
-        core.info(`Changed the status of ${task_id} successfully.`)
+        core.info(`Changed the status of ${task_id} to ${new_status} successfully.`)
     } catch (error) {
         core.setFailed(`Action failed: ${error}`)
     }
